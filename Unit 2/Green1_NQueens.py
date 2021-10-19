9177119c7a9c3aa3f87fd4b37d1e8c3ba6240e08
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-10-19 14:30:43 -0400

    finished NQueens base

--- Unit 2/Green1_NQueens.py
+++ Unit 2/Green1_NQueens.py
@@ -1,8 +1,10 @@
 from copy import copy
 
 def csp_backtracking(state):
-    if goal_test(state):
-        return state
+    if None not in state:
+        if test_solution(state):
+            print(test_solution(state))
+            return state
     var = get_next_unassigned_var(state)
     for val in get_sorted_values(state, var):
         new_state = copy(state)
@@ -13,7 +15,10 @@ def csp_backtracking(state):
     return None
 
 def get_next_unassigned_var(state): 
-    return state.index(None)
+    try: 
+        return state.index(None)
+    except:
+        pass
 
 def get_sorted_values(state, var):
     non_allowed = set()
@@ -22,31 +27,48 @@ def get_sorted_values(state, var):
     for index, elem in enumerate(state):
         if elem is not None:
             non_allowed.add(elem)
-            non_allowed.add((var - index) + elem) 
-            non_allowed.add((var - index) - elem) 
-    
-    # print(f"allowed: {allowed}")
-    # print(f"not allowed: {non_allowed}")
+            non_allowed.add(abs(var - index) + elem) 
+            non_allowed.add(elem - abs(var - index))
     for n in non_allowed:
-        if n >= 0 and n < 8:
+        if n >= 0 and n < len(state):
             allowed.remove(n)
     return allowed
     
 def create_board(size):
     return [None for x in range(size)]
 
-def goal_test(state):
-    # size = len(state)
-    # match = 0 
-    # for i in state:
-    #     for n in state:
-    #         if i == n:
-    #             match += 1 
-    #     if match != 1:
-    #         return False
-    #     match = 0
-    if state == [0, 4, 7, 5, 2, 6, 1, 3]:
-        return True
-    return False
+# def goal_test(state):
+#     # size = len(state)
+#     # match = 0 
+#     # for i in state:
+#     #     for n in state:
+#     #         if i == n:
+#     #             match += 1 
+#     #     if match != 1:
+#     #         return False
+#     #     match = 0
+#     if state == [0, 4, 7, 5, 2, 6, 1, 3]:
+#         return True
+#     return False
+
+def test_solution(state):
+    for var in range(len(state)):
+        left = state[var]
+        middle = state[var]
+        right = state[var]
+        for compare in range(var + 1, len(state)):
+            left -= 1
+            right += 1
+            if state[compare] == middle:
+                print(var, "middle", compare)
+                return False
+            if left >= 0 and state[compare] == left:
+                print(var, "left", compare)
+                return False 
+            if right < len(state) and state[compare] == right:
+                print(var, "right", compare)
+                return False
+    return True
+
 
 print(csp_backtracking(create_board(8)))
